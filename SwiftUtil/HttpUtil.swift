35555ca2316d7c782f6733aa22ba0babc577fbf1
chaluemwut <chaluemwut@gmail.com>
2015-06-06 18:23:28 +0700

    Add http file

---
+++ SwiftUtil/HttpUtil.swift
@@ -0,0 +1,19 @@
+//
+//  HttpUtil.swift
+//  SwiftUtil
+//
+//  Created by chaluemwut noyunsan on 6/6/2558 BE.
+//  Copyright (c) 2558 chaluemwut noyunsan. All rights reserved.
+//
+
+import Foundation
+
+public class HttpUtil {
+    public init(){
+        
+    }
+    
+    func aysn(strURL:String) {
+        println("test")
+    }
+}
