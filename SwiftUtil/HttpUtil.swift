c2e81ec1d37f4859045acb75db3a82a89a8c34e6
chaluemwut <chaluemwut@gmail.com>
2015-06-13 08:50:56 +0700

    ays http util

--- SwiftUtil/HttpUtil.swift
+++ SwiftUtil/HttpUtil.swift
@@ -6,7 +6,10 @@ public class HttpUtil {
         
     }
     
-    public func aysn(strURL:String) {
-        println("test")
+    public static func aysn(strURL:String, instance:AnyObject) {
+        var url: NSURL = NSURL(string: strURL)!
+        var request: NSURLRequest = NSURLRequest(URL: url)
+        var connection: NSURLConnection = NSURLConnection(request: request, delegate: instance, startImmediately: true)!
+        connection.start()
     }
 }
