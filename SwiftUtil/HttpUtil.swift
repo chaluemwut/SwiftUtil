f2946b63d0c6cbf1d3eef98078187a6cd980b69a
chaluemwut <chaluemwut@gmail.com>
2015-06-13 19:43:38 +0700

    change http util

--- SwiftUtil/HttpUtil.swift
+++ SwiftUtil/HttpUtil.swift
@@ -2,11 +2,17 @@ import Foundation
 
 public class HttpUtil {
     
-    public init(){
-        
+    public init(){}
+    
+    public static func request(strURL:String, instance:AnyObject) {
+        self.templateCall(strURL, instance: instance)
     }
     
-    public static func aysn(strURL:String, instance:AnyObject) {
+    public static func request(strURL:String){
+        self.templateCall(strURL, instance: nil)
+    }
+    
+    private static func templateCall(strURL:String, instance:AnyObject?){
         var url: NSURL = NSURL(string: strURL)!
         var request: NSURLRequest = NSURLRequest(URL: url)
         var connection: NSURLConnection = NSURLConnection(request: request, delegate: instance, startImmediately: true)!
