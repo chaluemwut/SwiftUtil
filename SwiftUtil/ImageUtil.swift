f244e9eb476ff89dc9a6d7f70097863059de58b0
chaluemwut <chaluemwut@gmail.com>
2015-06-15 17:04:50 +0700

    add image util

---
+++ SwiftUtil/ImageUtil.swift
@@ -0,0 +1,34 @@
+//
+//  ImageUtil.swift
+//  SwiftUtil
+//
+//  Created by chaluemwut noyunsan on 6/15/2558 BE.
+//  Copyright (c) 2558 chaluemwut noyunsan. All rights reserved.
+//
+
+import UIKit
+
+public class ImageUtil: NSObject {
+ 
+    
+    public func imageResize(imageObj:UIImage, sizeChange:CGSize)-> UIImage{
+        let hasAlpha = false
+        let scale: CGFloat = 0.0
+        UIGraphicsBeginImageContextWithOptions(sizeChange, !hasAlpha, scale)
+        imageObj.drawInRect(CGRect(origin: CGPointZero, size: sizeChange))
+        let scaledImage = UIGraphicsGetImageFromCurrentImageContext()
+        return scaledImage
+    }
+    
+    public func imageResize(imageObj:UIImage, reduceSize:CGFloat)-> UIImage{
+        let hasAlpha = false
+        let scale: CGFloat = 0.0
+        let newHeight:CGFloat = imageObj.size.height/reduceSize
+        let newWeigth:CGFloat = imageObj.size.width/reduceSize
+        let sizeChange:CGSize = CGSizeMake(newWeigth, newHeight)
+        UIGraphicsBeginImageContextWithOptions(sizeChange, !hasAlpha, scale)
+        imageObj.drawInRect(CGRect(origin: CGPointZero, size: sizeChange))
+        let scaledImage = UIGraphicsGetImageFromCurrentImageContext()
+        return scaledImage
+    }
+}
