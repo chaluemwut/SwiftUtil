04c48881aecf6346260cc0ef058bac9678ad688e
chaluemwut <chaluemwut@gmail.com>
2015-07-30 00:35:12 +0700

    change random value

--- SwiftUtil/HttpUtil.swift
+++ SwiftUtil/HttpUtil.swift
@@ -5,12 +5,12 @@ public class HttpUtil {
     public init(){}
     
     public static func request(var strURL:String, instance:AnyObject?) {
-        strURL+="?r="+RandomUtil.randomStringWithLength(5)
+        strURL=strURL+"&r="+RandomUtil.randomStringWithLength(5)
         self.templateCall(strURL, instance: instance)
     }
     
     public static func request(var strURL:String){
-        strURL+="?r="+RandomUtil.randomStringWithLength(5)
+        strURL=strURL+"&r="+RandomUtil.randomStringWithLength(5)
         self.templateCall(strURL, instance: nil)
     }
     
