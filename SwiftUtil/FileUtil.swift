4730df9313e95094f2283a27d84a6e86df846ed5
chaluemwut <chaluemwut@gmail.com>
2015-07-31 10:39:48 +0700

    add file util

--- SwiftUtil/FileUtil.swift
+++ SwiftUtil/FileUtil.swift
@@ -7,3 +7,24 @@
 //
 
 import Foundation
+
+public class FileUtil {
+    
+    public static func writeFile(msg:String, fileName:String){
+        if let dirs:[String] = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.AllDomainsMask, true) as? [String] {
+            let dir = dirs[0]
+            let path = dir.stringByAppendingPathComponent(fileName)
+            msg.writeToFile(path, atomically: false, encoding: NSUTF8StringEncoding, error: nil);
+        }
+    }
+    
+    public static func readFile(fileName:String) -> String? {
+        if let dirs:[String] = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.AllDomainsMask, true) as? [String] {
+            let dir = dirs[0]
+            let path = dir.stringByAppendingPathComponent(fileName)
+            let ret:String? = String(contentsOfFile: path, encoding: NSUTF8StringEncoding, error: nil)
+            return ret
+        }
+        return nil
+    }
+}
