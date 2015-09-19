a69e25b973952254d0dbcdf038d6fd5e53cffef3
chaluemwut <chaluemwut@gmail.com>
2015-09-19 15:59:31 +0700

    fix sdk9

--- SwiftUtil/FileUtil.swift
+++ SwiftUtil/FileUtil.swift
@@ -11,41 +11,17 @@ import Foundation
 public class FileUtil {
     
     public static func writeFile(msg:String, fileName:String){
-        if let dirs:[String] = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.AllDomainsMask, true) as? [String] {
-            let dir = dirs[0]
-            let path = dir.stringByAppendingPathComponent(fileName)
-            do {
-                try msg.writeToFile(path, atomically: false, encoding: NSUTF8StringEncoding)
-            } catch _ {
-            };
-        }
+
     }
     
     public static func readFile(fileName:String) -> String? {
-        if let dirs:[String] = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.AllDomainsMask, true) as? [String] {
-            let dir = dirs[0]
-            let path = dir.stringByAppendingPathComponent(fileName)
-            let ret:String? = try? String(contentsOfFile: path, encoding: NSUTF8StringEncoding)
-            return ret
-        }
-        return nil
+        return nil;
     }
     
     public static func writeImage(img:UIImage, fileName:String){
-        if let dirs:[String] = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.AllDomainsMask, true) as? [String] {
-            let dir = dirs[0]
-            let path = dir.stringByAppendingPathComponent(fileName)
-            UIImageJPEGRepresentation(img, 1.0).writeToFile(path, atomically: true)
-        }
     }
     
     public static func readImag(fileName:String) -> UIImage? {
-        if let dirs:[String] = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.AllDomainsMask, true) as? [String] {
-            let dir = dirs[0]
-            let path = dir.stringByAppendingPathComponent(fileName)
-            let ret:UIImage? = UIImage(contentsOfFile: path)
-            return ret
-        }
         return nil
     }
 }
