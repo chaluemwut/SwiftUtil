b8108ae90738018a2ff081e99df26f3fe6f699f2
chaluemwutnoyunsan <chaluemwut@hotmail.com>
2016-05-11 21:59:14 +0700

    add iPhone 6 plus

--- SwiftUtil/UIUtil.swift
+++ SwiftUtil/UIUtil.swift
@@ -8,6 +8,15 @@
 
 import Foundation
 
+struct ScreenSize
+{
+    static let SCREEN_WIDTH = UIScreen.mainScreen().bounds.size.width
+    static let SCREEN_HEIGHT = UIScreen.mainScreen().bounds.size.height
+    static let SCREEN_MAX_LENGTH = max(ScreenSize.SCREEN_WIDTH, ScreenSize.SCREEN_HEIGHT)
+    static let SCREEN_MIN_LENGTH = min(ScreenSize.SCREEN_WIDTH, ScreenSize.SCREEN_HEIGHT)
+}
+
+
 public class UIUtil {
     
     public static func standardButton(rec: CGRect, msg:String, color:UIColor) -> UIButton {
@@ -21,10 +30,11 @@ public class UIUtil {
         return btn
     }
     
-    public static func getSize(iphone: CGRect, ipad: CGRect) -> CGRect {
+    public static func getSize(iphone: CGRect, iphone6plus: CGRect, ipad: CGRect) -> CGRect {
         if (UIDevice.currentDevice().userInterfaceIdiom == UIUserInterfaceIdiom.Pad){
-            // Ipad
             return ipad
+        } else if  UIDevice.currentDevice().userInterfaceIdiom == UIUserInterfaceIdiom.Phone && ScreenSize.SCREEN_MAX_LENGTH == 736.0{
+            return iphone6plus
         } else {
             // Iphone
             return iphone
